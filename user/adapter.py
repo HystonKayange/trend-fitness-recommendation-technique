c5a96acaa76510b79952dd42579575f160ddf90b
nathius262 <egbodonathaniel@yahoo.com>
2024-07-07 08:22:47 +0100

    modified user profile

--- user/adapter.py
+++ user/adapter.py
@@ -4,5 +4,5 @@ from allauth.account.adapter import DefaultAccountAdapter
 class MyAccountAdapter(DefaultAccountAdapter):
 
     def get_signup_redirect_url(self, request):
-        path = "/account/profile/{username}"
+        path = "/account/profile/{username}/"
         return path.format(username=request.user.username)
--- user/wizards.py
+++ user/wizards.py
@@ -1,6 +1,6 @@
 from formtools.wizard.views import SessionWizardView
 from .forms import ProfileFormSection1, ProfileFormSection2
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import redirect, get_object_or_404
 from .models import Profile
 
@@ -24,20 +24,22 @@ class MyWizard(SessionWizardView):
     def done(self, form_list, form_dict, **kwargs):
         user = self.request.user
 
-        # Iterate over the form list and save the data
-        for form in form_list:
+        # Check if all forms are valid
+        if all(form.is_valid() for form in form_dict.values()):
             # Check if an instance already exists for the user
             instance, created = Profile.objects.get_or_create(user=user)
 
-            # Bind the user to the instance
-            form.instance.user = user
+            # Iterate over the form list and save the data to the same instance
+            for form_key, form in form_dict.items():
+                form.instance = instance  # Assign the existing instance to the form
+                form.save(commit=False)
 
-            # Update the instance with the form data
-            print(form)
-            form.save(commit=False)
-            if not created:
-                form.instance.pk = instance.pk  # Set the primary key to update existing instance
-            form.save()
+                # Iterate over the cleaned data of the form and set it on the instance
+                for field, value in form.cleaned_data.items():
+                    setattr(instance, field, value)
+
+            # After all forms are processed, save the instance
+            instance.save()
 
             # Perform additional actions
             print("All forms are valid. Data saved successfully.")
@@ -45,6 +47,8 @@ class MyWizard(SessionWizardView):
         else:
             # At least one form is invalid, return to the previous step
             return self.render_revalidation_failure(form_list, form_dict, **kwargs)
+
+
 
     def render_revalidation_failure(self, form_list, form_dict, **kwargs):
         # Find the first invalid form
